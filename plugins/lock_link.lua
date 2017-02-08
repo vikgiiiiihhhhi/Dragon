d0a13045c79ce8794ed51d9673cd194f54a7ef8a
Ali-Dragon <aa2304862@gmail.com>
2017-02-08 05:21:13 +0300

    Add files via upload

---
+++ plugins/lock_link.lua
@@ -0,0 +1,59 @@
+--[[ 
+     _        BY:@Aaa1R                     
+  __| |_ __ __ _  __ _  ___  _ __  
+ / _` | '__/ _` |/ _` |/ _ \| '_ \ 
+| (_| | | | (_| | (_| | (_) | | | |
+ \__,_|_|  \__,_|\__, |\___/|_| |_|
+                 |___/             
+--]]
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_link'] then
+                lock_ads = data[tostring(msg.to.id)]['settings']['lock_link']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_ads == "yes" then
+       delete_msg(msg.id, ok_cb, true)
+    end
+end
+
+return {patterns = {
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Hh][Tt][Tt][Pp]://",
+"[Ww][Ww][Ww]:",
+"telegram.me/(.*)",
+"TELEGRAM.ME/(.*)",
+"Telegram.me/(.*)",
+"(.*) telegram.me/(.*)",
+"(.*) https://(.*)",
+"(.*) www.(.*)",
+"(.*) telegram.me/(.*)",
+"(.*) https://(.*)",
+"(.*) www.(.*)";
+"https://(.*)",
+"http://(.*)",
+"www.(.*)",
+"WWW.(.*)",
+
+  },
+  run = run
+}
+--[[ 
+     _        BY:@Aaa1R                     
+  __| |_ __ __ _  __ _  ___  _ __  
+ / _` | '__/ _` |/ _` |/ _ \| '_ \ 
+| (_| | | | (_| | (_| | (_) | | | |
+ \__,_|_|  \__,_|\__, |\___/|_| |_|
+                 |___/             
+--]]
