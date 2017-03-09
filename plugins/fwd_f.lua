ad966fd3c375596117124290992b52e5df53862a
vikgiiiiihhhhi <zazxi@bareed.ws>
2017-03-10 02:38:33 +0300

    Update fwd_f.lua

--- plugins/fwd_f.lua
+++ plugins/fwd_f.lua
@@ -1,53 +1,52 @@
-local function pre_process(msg) 
-  local jalal = msg['id'] 
-  local user = msg.from.id 
-  local chat = msg.to.id 
-  local hash = 'mate:'..chat..':'..user 
-  if msg.fwd_from and not is_momod(msg) then 
-    if redis:get(hash) and msg.fwd_from and not is_momod(msg) then 
-      delete_msg(msg.id, ok_cb, false) 
-      delete_msg(msg.id, ok_cb, false) 
-      delete_msg(msg.id, ok_cb, false) 
-      send_large_msg(get_receiver(msg), '#ركــز_جــرجــف🌝✋مـمنوع ســوي تـوجيه اهـنـا🍹🌚لابــل نـعـال ☹️🖕🏿\n #الـــعــــضـــو : @'..msg.from.username)
-      redis:del(hash) 
-   else 
-      delete_msg(msg.id, ok_cb, false) 
-      redis:set(hash, true) 
-    end 
-  end 
-  return  msg 
-end 
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY SAJJAD HUSSIEN                 ▀▄ ▄▀ 
+▀▄ ▄▀   BY SAJJADHUSSIEN   (@TH3_Evil)     ▀▄ ▄▀    
+▀▄ ▄  JUST WRITED BY SAJJAD HUSSIEN        ▀▄ ▄▀ 
+▀▄ ▄▀   ANTI FWD  :  منع اعاده توجيه      ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+do
 
-local function run(msg, matches) 
-  local jalal = msg['id'] 
-  if matches[1] == 'قفل اعاده توجيه' then 
-    if is_momod(msg) then 
-      local hash = 'mate:'..msg.to.id 
-      redis:set(hash, true) 
-         local x = "" 
-      reply_msg(jalal, x, ok_cb, true) 
-    else 
-      local asdy = 'هاذ الامر للمشرفين #فقط❗️⛔️' 
-      reply_msg(jalal, asdy, ok_cb, true) 
-    end 
-  end 
-  if matches[1] == 'فتح اعاده توجيه' then 
-    if is_momod(msg) then 
-      local hash = 'mate:'..msg.to.id 
-      redis:del(hash) 
-    local don = "" 
-      reply_msg(jalal, don, ok_cb, true) 
-    else 
-      local jalal_aldon = 'هاذ الامر للمشرفين #فقط❗️⛔️' 
-      reply_msg(jalal, jalal_aldon, ok_cb, true) 
-    end 
-  end 
-end 
-return { 
-    patterns = { 
-        "^(قفل اعاده توجيه)$", 
-        "^(فتح اعاده توجيه)$" 
-    }, 
-run = run, 
-    pre_process = pre_process 
+local function pre_process(msg)
+    
+    --Checking mute
+    local hash = 'mate:'..msg.to.id
+    if redis:get(hash) and msg.fwd_from and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg)  then
+            delete_msg(msg.id, ok_cb, true)
+            return "done"
+        end
+    
+        return msg
+    end
+
+  
+
+
+local function run(msg, matches)
+    chat_id =  msg.to.id
+    
+    if is_momod(msg) and matches[1] == 'قفل اعاده توجيه' then
+      
+            
+                    local hash = 'mate:'..msg.to.id
+                    redis:set(hash, true)
+                    return "تم ☑️ قفل 🔒 اعاده توجيه ✋😽"
+  elseif is_momod(msg) and matches[1] == 'فتح اعاده توجيه' then
+      local hash = 'mate:'..msg.to.id
+      redis:del(hash)
+      return "تم ☑️ فتح 🔓 اعاده توجيه ✋😽"
+end
+
+end
+
+return {
+    patterns = {
+        '^(قفل اعاده توجيه)$',
+        '^(فتح اعاده توجيه)$'
+    },
+    run = run,
+    pre_process = pre_process
 }
+end
